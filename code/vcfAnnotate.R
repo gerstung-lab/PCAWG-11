5a54c97f606bf5391aaf86058faaac6fdf54a582
Moritz Gerstung <moritz.gerstung@ebi.ac.uk>
2016-03-29 20:35:54 +0100

    Mock up BB if missing

--- code/vcfAnnotate.R
+++ code/vcfAnnotate.R
@@ -31,7 +31,13 @@ print(ID)
 clusters <- loadClusters(ID)
 
 # Load BB
-bb <- loadBB(ID) 
+bb <- try(loadBB(ID))
+if(class(bb)=="try-error"){ # Mock up BB
+	m <- read.table(gzfile(paste0(basePath, "/0_multiplicity/",ID,"_multiplicity.txt.gz")), header=TRUE)
+	bb <- GRanges(m$chr, IRanges(m$pos, width=1), copy_number=m$tumour_copynumber, major_cn=m$nMaj1, minor_cn=m$nMin1, clonal_frequency=purityPloidy[ID,'purity'])
+	meta(header(vcf)) <- rbind(meta(header(vcf)), DataFrame(Value="False", row.names="Battenberg"))
+}
+	
 
 # Load vcf
 vcf <- readVcf(vcfFileName, genome="GRCh37") #, param=ScanVcfParam(which=pos))
