8b680c08b99564357441c533ea19a28117501232
Moritz Gerstung <moritz.gerstung@ebi.ac.uk>
2016-04-01 21:17:00 +0100

    Updated purity_ploidy

--- code/functions.R
+++ code/functions.R
@@ -7,7 +7,7 @@ vcfPath <- '/nfs/users/nfs_c/cgppipe/pancancer/workspace/mg14/subs/2016-03/annot
 basePath <-  '/nfs/users/nfs_c/cgppipe/pancancer/workspace/mg14/dp/2016_04_01_vanloo_wedge_consensusSNV_battenbergCNA'
 dpPath <- paste0(basePath,'/2_subclones/')
 cancerGenes <- read.table('/nfs/users/nfs_c/cgppipe/pancancer/workspace/mg14/ref/cancer_genes.txt')$V1
-purityPloidy <- read.table(paste0(basePath,'/1_purity_ploidy/purity_ploidy.txt'), header=FALSE, row.names=1)
+purityPloidy <- read.table(paste0(basePath,'/1_purity_ploidy/purity_ploidy.txt'), header=TRUE, row.names=1)
 colnames(purityPloidy) <- c("purity","ploidy")
 cnPath <- paste0(basePath,'/4_copynumber/')
 bbPath <- paste0(basePath,'/4_copynumber/')
