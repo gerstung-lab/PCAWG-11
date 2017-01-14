121746d0d265ab997317bb6ec71c6cec2ad4ce97
Moritz Gerstung <moritz.gerstung@ebi.ac.uk>
2017-01-14 15:05:28 +0000

    DPFILE

--- code/vcfAnnotateFinal.sh
+++ code/vcfAnnotateFinal.sh
@@ -15,8 +15,8 @@ FILES=(`ls $INPUT_FOLDER/*.vcf.gz`)
 INPUT=${FILES[(($LSB_JOBINDEX-1))]}
 echo $INPUT
 STEM=`basename $INPUT | sed s/.vcf.gz//g`
-ID=`echo $STEM | awk '{x=$0; gsub("\\..*","",x); print x}'`
-DPFILE="../dp/20161213_vanloo_wedge_consSNV_prelimConsCNAallStar/2_subclones/$ID_subclonal_structure.txt.gz"
+ID=`echo $STEM | awk '{x=$0; gsub("\\\..*","",x); print x}'`
+DPFILE="../dp/20161213_vanloo_wedge_consSNV_prelimConsCNAallStar/2_subclones/${ID}_subclonal_structure.txt.gz"
 if [ ! -f "$DPFILE" ]; then
 echo "No DP file $DPFILE. Exit."
 exit 0
