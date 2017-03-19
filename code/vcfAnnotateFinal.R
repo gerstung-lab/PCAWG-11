40eff240b45a4af46796b4737a4cfcb43826afb8
Moritz Gerstung <moritz.gerstung@ebi.ac.uk>
2017-03-19 20:58:18 +0000

    With 200 bootstraps

--- code/vcfAnnotateFinal.R
+++ code/vcfAnnotateFinal.R
@@ -94,7 +94,7 @@ if(!"TNC" %in% rownames(header(vcf)@header$INFO)){
 # vcf <-  addMutCn(vcf, bb, clusters)
 i = header(vcf)@header$INFO
 exptData(vcf)$header@header$INFO <- rbind(i,mcnHeader())
-L <- computeMutCn(vcf, bb, clusters=clusters, purity=purity, xmin=3, gender=as.character(allGender[ID, "pred_gender"]), isWgd=IS_WGD, n.boot=10)
+L <- computeMutCn(vcf, bb, clusters=clusters, purity=purity, xmin=3, gender=as.character(allGender[ID, "pred_gender"]), isWgd=IS_WGD)
 info(vcf) <- cbind(info(vcf), L$D)
 bb$timing_param <- L$P 
 
