d1ed7317dff848dbaabfaa9029b18fc464b30b7b
Moritz Gerstung <moritz.gerstung@ebi.ac.uk>
2018-01-17 16:05:24 +0000

    Changed

--- code/Hotspots-bam2.R
+++ code/Hotspots-bam2.R
@@ -7,12 +7,12 @@ library(deepSNV)
 
 args <- commandArgs(trailingOnly = TRUE)
 
+tab <- read.table("Protected_sites_shearwater_normalpanel_after_filteringSNPs.txt", sep="\t", header=FALSE)
 setwd("/lustre/scratch112/sanger/cgppipe/PanCancerDownloads/workspace/mg14/bam/tumour")
 
 
 i <- as.numeric(args[1])
 
-tab <- read.table("Protected_sites_shearwater_normalpanel_after_filteringSNPs.txt", sep="\t", header=FALSE)
 sites <- GRanges(tab$V1, IRanges(tab$V2, tab$V2))
 
 files = dir(".", pattern=".bam$")
