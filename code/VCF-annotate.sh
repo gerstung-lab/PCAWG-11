b5ae5999a80a42e6fc564d78c2a82e4310006abd
Moritz Gerstung <moritz.gerstung@ebi.ac.uk>
2018-01-26 14:32:50 +0000

    Fix folder name

--- code/VCF-annotate.sh
+++ code/VCF-annotate.sh
@@ -8,7 +8,7 @@
 #BSUB -n 1
 
 INPUT_FOLDER="../final/final_consensus_12oct_passonly/graylist/snv_mnv"
-OUTPUT_FOLDER="../final/annotated_011/snv_mnv/graylist"
+OUTPUT_FOLDER="../final/annotated_011/graylist/snv_mnv"
 OVERWRITE=true
 
 FILES=(`ls $INPUT_FOLDER/*.vcf.gz`)
